dd02b491c85f85007b6ee0afd9942be3a486a5d0
Francis Pion <francispion@hotmail.com>
2026-03-22 13:29:15 -0400

    Configured OpenAPI. (#5)

---
+++ src/PokeGame/Controllers/IndexController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokeGame.Models.Index;
+using PokeGame.Settings;
+
+namespace PokeGame.Controllers;
+
+[ApiController]
+[Route("")]
+public class IndexController : ControllerBase
+{
+  private readonly ApiSettings _settings;
+
+  public IndexController(ApiSettings settings)
+  {
+    _settings = settings;
+  }
+
+  [HttpGet]
+  public ActionResult<ApiVersion> Get()
+  {
+    return Ok(new ApiVersion(_settings.Title, _settings.Version));
+  }
+}
---
+++ src/PokeGame/Extensions/AddHeaderParameters.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace PokeGame.Extensions;
+
+internal class AddHeaderParameters : IOperationFilter
+{
+  public void Apply(OpenApiOperation operation, OperationFilterContext context)
+  {
+    operation.Parameters?.Add(new OpenApiParameter
+    {
+      In = ParameterLocation.Header,
+      Name = "X-World", // TODO(fpion): ResolveWorld.Header,
+      Description = "Enter your world ID in the input below:"
+    });
+  }
+}
