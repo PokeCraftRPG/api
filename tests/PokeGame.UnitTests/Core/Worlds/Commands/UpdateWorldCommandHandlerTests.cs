0d0e44ca01189bc091c1ba0b6b48d96b2892a63b
Francis Pion <francispion@hotmail.com>
2026-03-22 16:00:04 -0400

    unit

---
+++ tests/PokeGame.UnitTests/Core/Worlds/Commands/UpdateWorldCommandHandlerTests.cs
@@ -0,0 +1,79 @@
+﻿using Bogus;
+using Moq;
+using PokeGame.Builders;
+using PokeGame.Core.Permissions;
+using PokeGame.Core.Storages;
+using PokeGame.Core.Worlds.Models;
+
+namespace PokeGame.Core.Worlds.Commands;
+
+[Trait(Traits.Category, Categories.Unit)]
+public class UpdateWorldCommandHandlerTests
+{
+  private readonly CancellationToken _cancellationToken = default;
+  private readonly Faker _faker = new();
+
+  private readonly Mock<IContext> _context = new();
+  private readonly Mock<IPermissionService> _permissionService = new();
+  private readonly Mock<IStorageService> _storageService = new();
+  private readonly Mock<IWorldQuerier> _worldQuerier = new();
+  private readonly Mock<IWorldRepository> _worldRepository = new();
+
+  private readonly UpdateWorldCommandHandler _handler;
+
+  public UpdateWorldCommandHandlerTests()
+  {
+    _handler = new(_context.Object, _permissionService.Object, _storageService.Object, _worldQuerier.Object, _worldRepository.Object);
+  }
+
+  [Fact(DisplayName = "It should return null when the world does not exist.")]
+  public async Task Given_DoesNotExist_When_HandleAsync_Then_NullReturned()
+  {
+    UpdateWorldPayload payload = new();
+    UpdateWorldCommand command = new(Guid.Empty, payload);
+    Assert.Null(await _handler.HandleAsync(command, _cancellationToken));
+  }
+
+  [Fact(DisplayName = "It should throw ValidationException when the payload is not valid.")]
+  public async Task Given_InvalidPayload_When_HandleAsync_Then_ValidationException()
+  {
+    UpdateWorldPayload payload = new()
+    {
+      Key = "in--val!d",
+      Name = new Optional<string>(_faker.Random.String(Name.MaximumLength + 1, 'a', 'z'))
+    };
+    UpdateWorldCommand command = new(Guid.Empty, payload);
+
+    var exception = await Assert.ThrowsAsync<FluentValidation.ValidationException>(async () => await _handler.HandleAsync(command, _cancellationToken));
+    Assert.Equal(2, exception.Errors.Count());
+    Assert.Contains(exception.Errors, e => e.ErrorCode == "SlugValidator" && e.PropertyName == "Key");
+    Assert.Contains(exception.Errors, e => e.ErrorCode == "MaximumLengthValidator" && e.PropertyName == "Name.Value");
+  }
+
+  [Fact(DisplayName = "It should update the existing world.")]
+  public async Task Given_Exists_When_HandleAsync_Then_Replaced()
+  {
+    World world = new WorldBuilder(_faker).Build();
+    world.ClearChanges();
+    _worldRepository.Setup(x => x.LoadAsync(world.Id, _cancellationToken)).ReturnsAsync(world);
+
+    UpdateWorldPayload payload = new()
+    {
+      Key = "the-new-world",
+      Name = new Optional<string>("The New World"),
+      Description = new Optional<string>("This is the new world.")
+    };
+    UpdateWorldCommand command = new(world.Id.ToGuid(), payload);
+
+    WorldModel model = new();
+    _worldQuerier.Setup(x => x.ReadAsync(world, _cancellationToken)).ReturnsAsync(model);
+
+    WorldModel? result = await _handler.HandleAsync(command, _cancellationToken);
+    Assert.NotNull(result);
+    Assert.Same(model, result);
+
+    _permissionService.Verify(x => x.CheckAsync(Actions.Update, world, _cancellationToken), Times.Once());
+    _worldQuerier.Verify(x => x.EnsureUnicityAsync(world, _cancellationToken), Times.Once());
+    _storageService.Verify(x => x.ExecuteWithQuotaAsync(world, It.IsAny<Func<Task>>(), _cancellationToken), Times.Once());
+  }
+}
---
+++ tests/PokeGame.UnitTests/Core/Worlds/Queries/ReadWorldQueryHandlerTests.cs
@@ -0,0 +1,69 @@
+﻿using Bogus;
+using Krakenar.Contracts;
+using Moq;
+using PokeGame.Core.Worlds.Models;
+
+namespace PokeGame.Core.Worlds.Queries;
+
+[Trait(Traits.Category, Categories.Unit)]
+public class ReadWorldQueryHandlerTests
+{
+  private readonly Faker _faker = new();
+  private readonly CancellationToken _cancellationToken = default;
+
+  private readonly Mock<IWorldQuerier> _worldQuerier = new();
+
+  private readonly ReadWorldQueryHandler _handler;
+
+  public ReadWorldQueryHandlerTests()
+  {
+    _handler = new(_worldQuerier.Object);
+  }
+
+  [Fact(DisplayName = "It should return null when no world was found.")]
+  public async Task Given_NoneFound_When_ExecuteAsync_Then_NullReturned()
+  {
+    ReadWorldQuery query = new(Guid.Empty, "the-new-world");
+    Assert.Null(await _handler.HandleAsync(query, _cancellationToken));
+  }
+
+  [Fact(DisplayName = "It should return the world when it was found many times.")]
+  public async Task Given_SameFound_When_ExecuteAsync_Then_WorldReturned()
+  {
+    WorldModel world = new()
+    {
+      Id = Guid.NewGuid(),
+      Key = "the-new-world"
+    };
+    _worldQuerier.Setup(x => x.ReadAsync(world.Id, _cancellationToken)).ReturnsAsync(world);
+    _worldQuerier.Setup(x => x.ReadAsync(world.Key, _cancellationToken)).ReturnsAsync(world);
+
+    ReadWorldQuery query = new(world.Id, world.Key);
+    WorldModel? result = await _handler.HandleAsync(query, _cancellationToken);
+    Assert.NotNull(result);
+    Assert.Same(world, result);
+  }
+
+  [Fact(DisplayName = "It should throw TooManyResultsException when many worlds were found.")]
+  public async Task Given_ManyFound_When_ExecuteAsync_Then_TooManyResultsException()
+  {
+    WorldModel world1 = new()
+    {
+      Id = Guid.NewGuid(),
+      Key = "the-old-world"
+    };
+    _worldQuerier.Setup(x => x.ReadAsync(world1.Id, _cancellationToken)).ReturnsAsync(world1);
+
+    WorldModel world2 = new()
+    {
+      Id = Guid.NewGuid(),
+      Key = "the-new-world"
+    };
+    _worldQuerier.Setup(x => x.ReadAsync(world2.Key, _cancellationToken)).ReturnsAsync(world2);
+
+    ReadWorldQuery query = new(world1.Id, world2.Key);
+    var exception = await Assert.ThrowsAsync<TooManyResultsException<WorldModel>>(async () => await _handler.HandleAsync(query, _cancellationToken));
+    Assert.Equal(1, exception.ExpectedCount);
+    Assert.Equal(2, exception.ActualCount);
+  }
+}
