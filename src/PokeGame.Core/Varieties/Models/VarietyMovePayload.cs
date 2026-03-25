d91cc22f2233ae300af406f57328c40cf2b2b664
Francis Pion <francispion@hotmail.com>
2026-03-24 23:32:15 -0400

    todo

--- src/PokeGame.Core/Varieties/Models/VarietyMovePayload.cs
+++ src/PokeGame.Core/Varieties/Models/VarietyMovePayload.cs
@@ -1,9 +1,9 @@
-namespace PokeGame.Core.Varieties.Models;
+﻿namespace PokeGame.Core.Varieties.Models;
 
 public record VarietyMovePayload
 {
   public string Move { get; set; }
-  public int Level { get; set; }
+  public int Level { get; set; } // TODO(fpion): should be nullable, but not null in CreateOrReplace payload (or treated as 0)
 
   public VarietyMovePayload() : this(string.Empty, default)
   {
