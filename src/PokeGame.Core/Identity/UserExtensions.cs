11ce9406171c51c90f4834f61b33d11d204afe40
Francis Pion <francispion@hotmail.com>
2026-03-27 14:56:20 -0400

    fixes

--- src/PokeGame.Core/Identity/UserExtensions.cs
+++ src/PokeGame.Core/Identity/UserExtensions.cs
@@ -10,15 +10,43 @@ internal static class UserExtensions
 
   public static MultiFactorAuthenticationMode GetMultiFactorAuthenticationMode(this User user)
   {
-    CustomAttribute[] customAttributes = user.CustomAttributes.Where(x => x.Key == MultiFactorAuthenticationModeKey).ToArray();
-    return customAttributes.Length == 1
-      ? Enum.Parse<MultiFactorAuthenticationMode>(customAttributes.Single().Value)
-      : MultiFactorAuthenticationMode.None;
+    CustomAttribute? customAttribute = user.TryGetCustomAttribute(MultiFactorAuthenticationModeKey);
+    if (customAttribute is null)
+    {
+      return MultiFactorAuthenticationMode.None;
+    }
+    else if (Enum.TryParse(customAttribute.Value.Trim(), ignoreCase: true, out MultiFactorAuthenticationMode multiFactorAuthenticationMode) && Enum.IsDefined(multiFactorAuthenticationMode))
+    {
+      return multiFactorAuthenticationMode;
+    }
+    throw new ArgumentException($"The user does not have a valid '{MultiFactorAuthenticationModeKey}' custom attribute.", nameof(user));
   }
 
   public static bool IsProfileCompleted(this User user)
   {
-    CustomAttribute[] customAttributes = user.CustomAttributes.Where(x => x.Key == ProfileCompletedOnKey).ToArray();
-    return customAttributes.Length == 1 && DateTime.TryParse(customAttributes.Single().Value, out _);
+    CustomAttribute? customAttribute = user.TryGetCustomAttribute(ProfileCompletedOnKey);
+    if (customAttribute is null)
+    {
+      return false;
+    }
+    else if (DateTime.TryParse(customAttribute.Value.Trim(), CultureInfo.InvariantCulture, out _))
+    {
+      return true;
+    }
+    throw new ArgumentException($"The user does not have a valid '{ProfileCompletedOnKey}' custom attribute.", nameof(user));
+  }
+
+  private static CustomAttribute FindCustomAttribute(this User user, string key)
+  {
+    return user.TryGetCustomAttribute(key) ?? throw new ArgumentException($"The user '{user}' does not have a custom attribute '{key}'.", nameof(user));
+  }
+  private static CustomAttribute? TryGetCustomAttribute(this User user, string key)
+  {
+    CustomAttribute[] customAttributes = user.CustomAttributes.Where(x => x.Key == key).ToArray();
+    if (customAttributes.Length > 1)
+    {
+      throw new ArgumentException($"The user has many ({customAttributes.Length}) user attributes '{key}'.", nameof(user));
+    }
+    return customAttributes.SingleOrDefault();
   }
 }
---
+++ src/PokeGame/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokeGame.Core.Accounts;
+using PokeGame.Core.Accounts.Models;
+
+namespace PokeGame.Controllers;
+
+[ApiController]
+public class AccountController : ControllerBase
+{
+  private readonly IAccountService _accountService;
+
+  public AccountController(IAccountService accountService)
+  {
+    _accountService = accountService;
+  }
+
+  [HttpPost("/sign/in")]
+  public async Task<ActionResult<SignInAccountResult>> SignInAsync([FromBody] SignInAccountPayload payload, CancellationToken cancellationToken)
+  {
+    SignInAccountResult result = await _accountService.SignInAsync(payload, cancellationToken);
+    return Ok(result);
+  }
+}
