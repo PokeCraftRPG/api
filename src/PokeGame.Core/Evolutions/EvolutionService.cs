21affb249f3edf161d2cf77c2a5048ec53503c16
Francis Pion <francispion@hotmail.com>
2026-04-01 19:16:43 -0400

    controller+service

---
+++ src/PokeGame.Core/Evolutions/EvolutionService.cs
@@ -0,0 +1,35 @@
+﻿using Logitar.CQRS;
+using Microsoft.Extensions.DependencyInjection;
+using PokeGame.Core.Evolutions.Commands;
+using PokeGame.Core.Evolutions.Models;
+
+namespace PokeGame.Core.Evolutions;
+
+public interface IEvolutionService
+{
+  Task<CreateOrReplaceEvolutionResult> CreateOrReplaceAsync(CreateOrReplaceEvolutionPayload payload, Guid? id = null, CancellationToken cancellationToken = default);
+}
+
+internal class EvolutionService : IEvolutionService
+{
+  public static void Register(IServiceCollection services)
+  {
+    services.AddTransient<IEvolutionService, EvolutionService>();
+    services.AddTransient<ICommandHandler<CreateOrReplaceEvolutionCommand, CreateOrReplaceEvolutionResult>, CreateOrReplaceEvolutionCommandHandler>();
+  }
+
+  private readonly ICommandBus _commandBus;
+  private readonly IQueryBus _queryBus;
+
+  public EvolutionService(ICommandBus commandBus, IQueryBus queryBus)
+  {
+    _commandBus = commandBus;
+    _queryBus = queryBus;
+  }
+
+  public async Task<CreateOrReplaceEvolutionResult> CreateOrReplaceAsync(CreateOrReplaceEvolutionPayload payload, Guid? id, CancellationToken cancellationToken)
+  {
+    CreateOrReplaceEvolutionCommand command = new(payload, id);
+    return await _commandBus.ExecuteAsync(command, cancellationToken);
+  }
+}
---
+++ src/PokeGame/Controllers/EvolutionController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PokeGame.Core.Evolutions;
+using PokeGame.Core.Evolutions.Models;
+using PokeGame.Extensions;
+
+namespace PokeGame.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("evolutions")]
+public class EvolutionController : ControllerBase
+{
+  private readonly IEvolutionService _evolutionService;
+
+  public EvolutionController(IEvolutionService evolutionService)
+  {
+    _evolutionService = evolutionService;
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<EvolutionModel>> CreateAsync([FromBody] CreateOrReplaceEvolutionPayload payload, CancellationToken cancellationToken)
+  {
+    CreateOrReplaceEvolutionResult result = await _evolutionService.CreateOrReplaceAsync(payload, id: null, cancellationToken);
+    return ToActionResult(result);
+  }
+
+  [HttpPut("{id}")]
+  public async Task<ActionResult<EvolutionModel>> ReplaceAsync(Guid id, [FromBody] CreateOrReplaceEvolutionPayload payload, CancellationToken cancellationToken)
+  {
+    CreateOrReplaceEvolutionResult result = await _evolutionService.CreateOrReplaceAsync(payload, id, cancellationToken);
+    return ToActionResult(result);
+  }
+
+  private ActionResult<EvolutionModel> ToActionResult(CreateOrReplaceEvolutionResult result)
+  {
+    EvolutionModel evolution = result.Evolution;
+    if (result.Created)
+    {
+      Uri location = new($"{HttpContext.GetBaseUri()}/evolutions/{evolution.Id}", UriKind.Absolute);
+      return Created(location, evolution);
+    }
+    return Ok(evolution);
+  }
+}
