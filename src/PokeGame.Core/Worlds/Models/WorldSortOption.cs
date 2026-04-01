888aa9238ff91d29fc91fd5977b5c65f5e8eba42
Francis Pion <francispion@hotmail.com>
2026-04-01 16:05:03 -0400

    world

---
+++ src/PokeGame.Core/Worlds/Models/WorldSortOption.cs
@@ -0,0 +1,18 @@
+﻿using Krakenar.Contracts.Search;
+
+namespace PokeGame.Core.Worlds.Models;
+
+public record WorldSortOption : SortOption
+{
+  public new WorldSort Field
+  {
+    get => Enum.Parse<WorldSort>(base.Field);
+    set => base.Field = value.ToString();
+  }
+
+  public WorldSortOption(WorldSort field = WorldSort.Key, bool isDescending = false)
+  {
+    Field = field;
+    IsDescending = isDescending;
+  }
+}
