3f313e4cbaa3ef3645dd09adfaf25435ccba9fc0
Francis Pion <francispion@hotmail.com>
2026-04-14 16:44:01 -0400

    migration

---
+++ src/PokeGame.PostgreSQL/Migrations/20260414204309_AddPokemonSlotColumns.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokeGame.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPokemonSlotColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Box",
+                schema: "Pokemon",
+                table: "Pokemon",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Position",
+                schema: "Pokemon",
+                table: "Pokemon",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Box",
+                schema: "Pokemon",
+                table: "Pokemon");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                schema: "Pokemon",
+                table: "Pokemon");
+        }
+    }
+}
