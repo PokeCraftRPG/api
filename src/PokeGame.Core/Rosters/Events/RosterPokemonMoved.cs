a63c1140f69c1d43cc8b5071e6f19499b21be565
Francis Pion <francispion@hotmail.com>
2026-04-14 22:54:14 -0400

    rename

---
+++ src/PokeGame.Core/Rosters/Events/RosterPokemonMoved.cs
@@ -0,0 +1,6 @@
+﻿using Logitar.EventSourcing;
+using PokeGame.Core.Pokemon;
+
+namespace PokeGame.Core.Rosters.Events;
+
+public record RosterPokemonMoved(PokemonId PokemonId, PokemonSlot Slot) : DomainEvent;
