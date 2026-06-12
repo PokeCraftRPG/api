6ea31051f43f039e0dd41c7c39963f351ccf45fa
Francis Pion <francispion@hotmail.com>
2026-06-12 15:18:50 -0400

    infra

---
+++ src/PokeGame.Infrastructure/DependencyInjectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Logitar.EventSourcing.EntityFrameworkCore.Relational;
+using Logitar.EventSourcing.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using PokeGame.Core.Worlds;
+using PokeGame.Infrastructure.Repositories;
+
+namespace PokeGame.Infrastructure;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddPokeGameInfrastructure(this IServiceCollection services)
+  {
+    return services
+      .AddLogitarEventSourcingWithEntityFrameworkCoreRelational()
+      .AddRepositories()
+      .AddSingleton<IEventSerializer, EventSerializer>()
+      .AddScoped<IEventBus, EventBus>();
+  }
+
+  private static IServiceCollection AddRepositories(this IServiceCollection services)
+  {
+    return services.AddScoped<IWorldRepository, WorldRepository>();
+  }
+}
