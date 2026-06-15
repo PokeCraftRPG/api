f49e343aada7679302910dd7b2c680d4e3436e38
Francis Pion <francispion@hotmail.com>
2026-06-15 00:00:00 -0400

    migration

---
+++ src/PokeGame.PostgreSQL/Migrations/20260615035850_CreateMovesTable.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PokeGame.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateMovesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moves",
+                schema: "Pokemon",
+                columns: table => new
+                {
+                    MoveId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    WorldId = table.Column<int>(type: "integer", nullable: false),
+                    EntityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    Category = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    Key = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Accuracy = table.Column<byte>(type: "smallint", nullable: true),
+                    Power = table.Column<byte>(type: "smallint", nullable: true),
+                    PowerPoints = table.Column<byte>(type: "smallint", nullable: false),
+                    StreamId = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Version = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedBy = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moves", x => x.MoveId);
+                    table.ForeignKey(
+                        name: "FK_Moves_Worlds_WorldId",
+                        column: x => x.WorldId,
+                        principalSchema: "Pokemon",
+                        principalTable: "Worlds",
+                        principalColumn: "WorldId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_CreatedBy",
+                schema: "Pokemon",
+                table: "Moves",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_CreatedOn",
+                schema: "Pokemon",
+                table: "Moves",
+                column: "CreatedOn");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_StreamId",
+                schema: "Pokemon",
+                table: "Moves",
+                column: "StreamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_UpdatedBy",
+                schema: "Pokemon",
+                table: "Moves",
+                column: "UpdatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_UpdatedOn",
+                schema: "Pokemon",
+                table: "Moves",
+                column: "UpdatedOn");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_Version",
+                schema: "Pokemon",
+                table: "Moves",
+                column: "Version");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_WorldId_Accuracy",
+                schema: "Pokemon",
+                table: "Moves",
+                columns: new[] { "WorldId", "Accuracy" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_WorldId_Category",
+                schema: "Pokemon",
+                table: "Moves",
+                columns: new[] { "WorldId", "Category" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_WorldId_EntityId",
+                schema: "Pokemon",
+                table: "Moves",
+                columns: new[] { "WorldId", "EntityId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_WorldId_Key",
+                schema: "Pokemon",
+                table: "Moves",
+                columns: new[] { "WorldId", "Key" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_WorldId_Name",
+                schema: "Pokemon",
+                table: "Moves",
+                columns: new[] { "WorldId", "Name" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_WorldId_Power",
+                schema: "Pokemon",
+                table: "Moves",
+                columns: new[] { "WorldId", "Power" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_WorldId_PowerPoints",
+                schema: "Pokemon",
+                table: "Moves",
+                columns: new[] { "WorldId", "PowerPoints" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_WorldId_Type",
+                schema: "Pokemon",
+                table: "Moves",
+                columns: new[] { "WorldId", "Type" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Moves",
+                schema: "Pokemon");
+        }
+    }
+}
