5d5460703731acb8eca10b828771a4609120dad7
Francis Pion <francispion@hotmail.com>
2026-07-13 20:02:17 -0400

    core

---
+++ src/PokeGame.Core/Identity/IOneTimePasswordGateway.cs
@@ -0,0 +1,11 @@
+﻿using Krakenar.Contracts.Passwords;
+using Krakenar.Contracts.Users;
+using PokeGame.Core.Identity.Models;
+
+namespace PokeGame.Core.Identity;
+
+public interface IOneTimePasswordGateway
+{
+  Task<OneTimePassword> CreateMultiFactorAuthenticationAsync(User user, CancellationToken cancellationToken = default);
+  Task<User> ValidateMultiFactorAuthenticationAsync(OneTimePasswordValidation validation, CancellationToken cancellationToken = default);
+}
