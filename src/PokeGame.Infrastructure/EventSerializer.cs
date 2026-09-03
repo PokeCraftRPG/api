de1cfbb4986ada2bbd71913728d3664bd77a6ca7
Francis Pion <francispion@hotmail.com>
2026-09-02 20:28:43 -0400

    converters

--- src/PokeGame.Infrastructure/EventSerializer.cs
+++ src/PokeGame.Infrastructure/EventSerializer.cs
@@ -9,10 +9,13 @@ internal class EventSerializer : Logitar.EventSourcing.Infrastructure.EventSeria
     base.RegisterConverters();
 
     SerializerOptions.Converters.Add(new AbilityIdConverter());
+    SerializerOptions.Converters.Add(new AccuracyConverter());
     SerializerOptions.Converters.Add(new ContentConverter());
     SerializerOptions.Converters.Add(new KeyConverter());
     SerializerOptions.Converters.Add(new MoveIdConverter());
     SerializerOptions.Converters.Add(new NameConverter());
+    SerializerOptions.Converters.Add(new PowerConverter());
+    SerializerOptions.Converters.Add(new PowerPointsConverter());
     SerializerOptions.Converters.Add(new RegionIdConverter());
     SerializerOptions.Converters.Add(new SummaryConverter());
     SerializerOptions.Converters.Add(new UserIdConverter());
