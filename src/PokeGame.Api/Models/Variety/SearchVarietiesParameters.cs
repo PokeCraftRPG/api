249c179056d6b015594769aef05f02a80fe5b9d3
Francis Pion <francispion@hotmail.com>
2026-09-03 21:40:24 -0400

    web

---
+++ src/PokeGame.Api/Models/Variety/SearchVarietiesParameters.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokeGame.Api.Models.Search;
+using PokeGame.Core.Varieties.Models;
+
+namespace PokeGame.Api.Models.Variety;
+
+public record SearchVarietiesParameters : SearchParameters
+{
+  [FromQuery(Name = "species")]
+  public string? Species { get; set; }
+
+  [FromQuery(Name = "default")]
+  public bool? IsDefault { get; set; }
+
+  [FromQuery(Name = "metamorph")]
+  public bool? CanChangeForm { get; set; }
+
+  public virtual SearchVarietiesPayload ToPayload()
+  {
+    SearchVarietiesPayload payload = new();
+    payload.Species = Species;
+    payload.IsDefault = IsDefault;
+    payload.CanChangeForm = CanChangeForm;
+    Fill(payload);
+    return payload;
+  }
+}
