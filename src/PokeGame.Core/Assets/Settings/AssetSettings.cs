c115a35f5ec08137a7c87f0606f5a81fba44483a
Francis Pion <francispion@hotmail.com>
2026-09-04 09:57:03 -0400

    refactor

--- src/PokeGame.Core/Assets/Settings/AssetSettings.cs
+++ src/PokeGame.Core/Assets/Settings/AssetSettings.cs
@@ -1,3 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
 namespace PokeGame.Core.Assets.Settings;
 
 internal record AssetSettings(AssetKind Kind, string Extension);
+
+internal record AssetsSettings
+{
+  private const string SectionKey = "Assets";
+
+  public IReadOnlyDictionary<string, AssetSettings> SupportedTypes { get; init; } = new Dictionary<string, AssetSettings>().AsReadOnly();
+
+  public static AssetsSettings Initialize(IConfiguration configuration) => configuration.GetSection(SectionKey).Get<AssetsSettings>() ?? new();
+}
