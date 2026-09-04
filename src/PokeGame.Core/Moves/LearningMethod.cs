73736cc654f524a302c07c85fe12465f5555b6bf
Francis Pion <francispion@hotmail.com>
2026-09-03 22:42:32 -0400

    value_object

---
+++ src/PokeGame.Core/Moves/LearningMethod.cs
@@ -0,0 +1,8 @@
+﻿namespace PokeGame.Core.Moves;
+
+public enum LearningMethod
+{
+  LevelUp = 0,
+  Evolution = 1,
+  Reminder = 2
+}
---
+++ src/PokeGame.Core/Pokemon/Level.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace PokeGame.Core.Pokemon;
+
+public sealed class Level
+{
+  public int Value { get; }
+
+  public Level(int value)
+  {
+    Value = value;
+    new Validator().ValidateAndThrow(this);
+  }
+
+  public override bool Equals(object? obj) => obj is Level level && level.Value == Value;
+  public override int GetHashCode() => Value.GetHashCode();
+  public override string ToString() => Value.ToString();
+
+  private class Validator : AbstractValidator<Level>
+  {
+    public Validator()
+    {
+      RuleFor(x => x.Value).Level();
+    }
+  }
+}
