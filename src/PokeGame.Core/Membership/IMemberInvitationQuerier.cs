a645c157294df24338fcd3d0fb7b0f7a96d5ebb7
Francis Pion <francispion@hotmail.com>
2026-09-05 13:31:09 -0400

    next

---
+++ src/PokeGame.Core/Membership/IMemberInvitationQuerier.cs
@@ -0,0 +1,10 @@
+﻿using PokeGame.Core.Moves.Models;
+
+namespace PokeGame.Core.Membership;
+
+public interface IMemberInvitationQuerier
+{
+  Task<MoveDto> ReadAsync(MemberInvitation invitation, CancellationToken cancellationToken = default);
+  Task<MoveDto?> ReadAsync(MemberInvitationId id, CancellationToken cancellationToken = default);
+  Task<MoveDto?> ReadAsync(Guid id, CancellationToken cancellationToken = default);
+}
---
+++ src/PokeGame.Core/Membership/IMemberInvitationRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace PokeGame.Core.Membership;
+
+public interface IMemberInvitationRepository
+{
+  Task<MemberInvitation?> LoadAsync(MemberInvitationId id, CancellationToken cancellationToken = default);
+  Task<IReadOnlyCollection<MemberInvitation>> LoadAsync(IEnumerable<MemberInvitationId> ids, CancellationToken cancellationToken = default);
+
+  Task SaveAsync(MemberInvitation invitation, CancellationToken cancellationToken = default);
+  Task SaveAsync(IEnumerable<MemberInvitation> invitations, CancellationToken cancellationToken = default);
+}
