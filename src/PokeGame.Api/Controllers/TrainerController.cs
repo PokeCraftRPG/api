485d91caad2cc82a95933916c41b5b22ed939c2e
Francis Pion <francispion@hotmail.com>
2026-09-07 17:38:31 -0400

    web

---
+++ src/PokeGame.Api/Controllers/TrainerController.cs
@@ -0,0 +1,78 @@
+using Krakenar.Contracts.Search;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PokeGame.Api.Extensions;
+using PokeGame.Api.Filters;
+using PokeGame.Api.Models.Trainer;
+using PokeGame.Core.Trainers;
+using PokeGame.Core.Trainers.Models;
+
+namespace PokeGame.Api.Controllers;
+
+[ApiController]
+[Authorize]
+[RequireWorld]
+[Route("trainers")]
+public class TrainerController : ControllerBase
+{
+  private readonly ITrainerService _trainerService;
+
+  public TrainerController(ITrainerService trainerService)
+  {
+    _trainerService = trainerService;
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<TrainerDto>> CreateAsync([FromBody] CreateOrReplaceTrainerPayload payload, CancellationToken cancellationToken)
+  {
+    CreateOrReplaceTrainerResult result = await _trainerService.CreateOrReplaceAsync(payload, id: null, cancellationToken);
+    return ToActionResult(result);
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<TrainerDto>> ReadAsync(Guid id, CancellationToken cancellationToken)
+  {
+    TrainerDto? trainer = await _trainerService.ReadAsync(id, key: null, cancellationToken);
+    return trainer is null ? NotFound() : Ok(trainer);
+  }
+
+  [HttpGet("key:{key}")]
+  public async Task<ActionResult<TrainerDto>> ReadAsync(string key, CancellationToken cancellationToken)
+  {
+    TrainerDto? trainer = await _trainerService.ReadAsync(id: null, key, cancellationToken);
+    return trainer is null ? NotFound() : Ok(trainer);
+  }
+
+  [HttpPut("{id}")]
+  public async Task<ActionResult<TrainerDto>> ReplaceAsync(Guid id, [FromBody] CreateOrReplaceTrainerPayload payload, CancellationToken cancellationToken)
+  {
+    CreateOrReplaceTrainerResult result = await _trainerService.CreateOrReplaceAsync(payload, id, cancellationToken);
+    return ToActionResult(result);
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<SearchResults<TrainerDto>>> SearchAsync([FromQuery] SearchTrainersParameters parameters, CancellationToken cancellationToken)
+  {
+    SearchTrainersPayload payload = parameters.ToPayload();
+    SearchResults<TrainerDto> trainers = await _trainerService.SearchAsync(payload, cancellationToken);
+    return Ok(trainers);
+  }
+
+  [HttpPatch("{id}")]
+  public async Task<ActionResult<TrainerDto>> UpdateAsync(Guid id, [FromBody] UpdateTrainerPayload payload, CancellationToken cancellationToken)
+  {
+    TrainerDto? trainer = await _trainerService.UpdateAsync(id, payload, cancellationToken);
+    return trainer is null ? NotFound() : Ok(trainer);
+  }
+
+  private ActionResult<TrainerDto> ToActionResult(CreateOrReplaceTrainerResult result)
+  {
+    TrainerDto trainer = result.Trainer;
+    if (result.Created)
+    {
+      Uri location = new($"{HttpContext.GetBaseUrl()}/trainers/{trainer.Id}", UriKind.Absolute);
+      return Created(location, trainer);
+    }
+    return Ok(trainer);
+  }
+}
---
+++ src/PokeGame.Api/Models/Trainer/SearchTrainersParameters.cs
@@ -0,0 +1,14 @@
+﻿using PokeGame.Api.Models.Search;
+using PokeGame.Core.Trainers.Models;
+
+namespace PokeGame.Api.Models.Trainer;
+
+public record SearchTrainersParameters : SearchParameters
+{
+  public virtual SearchTrainersPayload ToPayload()
+  {
+    SearchTrainersPayload payload = new();
+    Fill(payload);
+    return payload;
+  }
+}
