d78d97fe23a73cdfb726411d2897894f3fba6671
Francis Pion <francispion@hotmail.com>
2026-09-08 19:36:22 -0400

    migration

---
+++ src/PokeGame.Infrastructure/Migrations/20260908233552_RequiredFormSize.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokeGame.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RequiredFormSize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Weight",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Height",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Weight",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Height",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
