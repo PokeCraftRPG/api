53f0b4330fac425d2b5700f895c26341232a52fa
Francis Pion <francispion@hotmail.com>
2026-09-09 15:54:32 -0400

    Changed forms integers to bytes.

---
+++ src/PokeGame.Infrastructure/Migrations/20260909195239_ChangeFormStatisticsToByte.cs
@@ -0,0 +1,234 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokeGame.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeFormStatisticsToByte : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "YieldSpeed",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "YieldSpecialDefense",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "YieldSpecialAttack",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "YieldHP",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "YieldDefense",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "YieldAttack",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "BaseSpeed",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "BaseSpecialDefense",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "BaseSpecialAttack",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "BaseHP",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "BaseDefense",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "BaseAttack",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "YieldSpeed",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "YieldSpecialDefense",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "YieldSpecialAttack",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "YieldHP",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "YieldDefense",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "YieldAttack",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BaseSpeed",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BaseSpecialDefense",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BaseSpecialAttack",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BaseHP",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BaseDefense",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BaseAttack",
+                schema: "Pokemon",
+                table: "Forms",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+        }
+    }
+}
