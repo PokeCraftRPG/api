b832f4318542d19c485b01bce151f2648bb54315
Francis Pion <francispion@hotmail.com>
2026-09-11 14:20:19 -0400

    fix

--- src/PokeGame.Core/Pokemon/PokemonOwnership.cs
+++ src/PokeGame.Core/Pokemon/PokemonOwnership.cs
@@ -5,7 +5,7 @@ using PokeGame.Core.Trainers;
 
 namespace PokeGame.Core.Pokemon;
 
-public record PokemonOwnership(OwnershipEvent Event, TrainerId TrainerId, ItemId PokeBallId, Level MetLevel, Location MetAt, DateTime MetOn)
+public sealed record PokemonOwnership(OwnershipEvent Event, TrainerId TrainerId, ItemId PokeBallId, Level MetLevel, Location MetAt, DateTime MetOn)
 {
   public static PokemonOwnership Caught(PokemonCaught @event) => new(OwnershipEvent.Caught, @event.TrainerId, @event.PokeBallId, @event.Level, @event.Location, @event.OccurredOn);
   public static PokemonOwnership Received(PokemonReceived @event) => new(OwnershipEvent.Received, @event.TrainerId, @event.PokeBallId, @event.Level, @event.Location, @event.OccurredOn);
