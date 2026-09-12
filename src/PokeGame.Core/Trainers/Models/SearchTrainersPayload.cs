17342073036da7046df63824d9c1bdf2fdd358c6
Francis Pion <francispion@hotmail.com>
2026-09-11 20:19:11 -0400

    trainer

--- src/PokeGame.Core/Trainers/Models/SearchTrainersPayload.cs
+++ src/PokeGame.Core/Trainers/Models/SearchTrainersPayload.cs
@@ -1,4 +1,5 @@
-﻿using PokeGame.Core.Search;
+﻿using FluentValidation;
+using PokeGame.Core.Search;
 
 namespace PokeGame.Core.Trainers.Models;
 
@@ -6,4 +7,16 @@ public record SearchTrainersPayload : SearchPayload<TrainerSort>
 {
   public Gender? Gender { get; set; }
   public Guid? MemberId { get; set; }
+
+  public override void Validate() => new Validator().ValidateQueryAndThrow(this);
+
+  private class Validator : AbstractValidator<SearchTrainersPayload>
+  {
+    public Validator()
+    {
+      Include(new SearchValidator<TrainerSort>());
+
+      RuleFor(x => x.Gender).IsInEnum();
+    }
+  }
 }
