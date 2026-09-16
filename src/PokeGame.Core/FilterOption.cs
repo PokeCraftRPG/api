6b2e4ac8879ee299f29fc9afc5a057c309bf86c1
Francis Pion <francispion@hotmail.com>
2026-09-16 16:19:01 -0400

    Species Filters. (#148)

---
+++ src/PokeGame.Core/FilterOption.cs
@@ -0,0 +1,3 @@
+namespace PokeGame.Core;
+
+public record FilterOption(string Text, string Value);
--- src/PokeGame.Core/Regions/IRegionQuerier.cs
+++ src/PokeGame.Core/Regions/IRegionQuerier.cs
@@ -7,6 +7,8 @@ public interface IRegionQuerier
 {
   Task<RegionId?> GetIdAsync(Key key, CancellationToken cancellationToken = default);
 
+  Task<IReadOnlyCollection<FilterOption>> ListOptionsAsync(CancellationToken cancellationToken = default);
+
   Task<RegionDto> ReadAsync(Region region, CancellationToken cancellationToken = default);
   Task<RegionDto?> ReadAsync(RegionId id, CancellationToken cancellationToken = default);
   Task<RegionDto?> ReadAsync(Guid id, CancellationToken cancellationToken = default);
